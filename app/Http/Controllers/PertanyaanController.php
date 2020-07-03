3515e5be7ed4a1b1d5da184e199eff31648b04eb
RevDonz <aprilio842@gmail.com>
2020-07-03 20:20:46 +0700

    Menambahkan fitur jawaban pada pertanyaan

--- app/Http/Controllers/PertanyaanController.php
+++ app/Http/Controllers/PertanyaanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\PertanyaanModel;
+use App\Models\JawabanModel;
 
 class PertanyaanController extends Controller
 {
--- resources/views/pertanyaan/detail.blade.php
+++ resources/views/pertanyaan/detail.blade.php
@@ -7,7 +7,7 @@
       <div class="card-header">
         <div class="user-block">
           <img class="img-circle" src="{{asset('/adminlte/dist/img/user1-128x128.jpg')}}" alt="User Image">
-          <span class="username"><a href="#">Jonathan Burke Jr.</a></span>
+          <span class="username"><a href="#">Reva Doni Aprilio</a></span>
           <span class="description">Shared publicly - 7:30 PM Today</span>
         </div>
 
@@ -16,18 +16,19 @@
       <div class="card-body">
         <!-- post text -->
         <p class="h3">
-			{{ $jawaban->judul }}
+			{{ $pertanyaan->judul }}
       	</p>
 
       	<p>
-			{{ $jawaban->isi }}
+			{{ $pertanyaan->isi }}
       	</p>
 
         <span class="float-right text-muted">45 likes - 2 comments</span>
       </div>
       <!-- /.card-body -->
       <div class="card-footer card-comments">
-
+		
+		@foreach ($jawaban as $jawab)
         <div class="card-comment">
           <!-- User image -->
         	<img class="img-circle img-sm" src="{{asset('/adminlte/dist/img/user3-128x128.jpg')}}" alt="User Image">
@@ -37,19 +38,26 @@
               		Maria Gonzales
               		<span class="text-muted float-right">8:03 PM Today</span>
             	</span>
-				{{ $jawaban->isi_jawaban }}
+            	{{ $jawab->isi_jawaban }}
           	</div>
-
         </div>
+        @endforeach
       </div>
 
       <!-- /.card-footer -->
       <div class="card-footer">
-        <form action="#" method="post">
-          <img class="img-fluid img-circle img-sm" src="{{asset('/adminlte/dist/img/user4-128x128.jpg')}}" alt="Alt Text">
+        <form action="/jawaban/" method="post">
+        	@csrf
+        	<input type="hidden" name="pertanyaan_id" id="pertanyaan_id" value="{{ $pertanyaan->id_pertanyaan }}">
+          <img class="img-fluid img-circle img-sm" src="{{asset('/adminlte/dist/img/user1-128x128.jpg')}}" alt="Alt Text">
           <!-- .img-push is used to add margin to elements next to floating images -->
-          <div class="img-push">
-            <input type="text" class="form-control form-control-sm" placeholder="Press enter to post comment">
+          <div class="img-push row">
+          	<div class="col-md-11">
+            	<textarea name="isi_jawaban" id="isi_jawaban" class="form-control" placeholder="Masukkan jawaban anda.."></textarea>
+          	</div>
+          	<div class="col-md-1">
+            	<input type="submit" class="btn btn-primary btn-block btn-sm" value="Jawab">
+          	</div>
           </div>
         </form>
       </div>
